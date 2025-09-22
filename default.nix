4dbd0dee71dc00084dba0cae9c1de09f6109ce74
Harendra Kumar <harendra@composewell.com>
2025-09-23 04:40:58 +0530

    Refactor and document default.nix

--- default.nix
+++ default.nix
@@ -1,146 +1,189 @@
-# Copyright   : (c) 2022 Composewell Technologies
-#
-# CAUTION! a spelling mistake in arg string is ignored silently.
-#
-# To use a specific ghc version:
-# nix-shell --argstr compiler "ghc966"
-# nix-shell --arg editors true --arg hoogle true
+# Copyright : (c) 2022 Composewell Technologies
 
 {
-  nixpkgs ?
-    import (builtins.fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/refs/tags/branch-off-24.11.tar.gz)
-      # Unfree for some vscode extensions
-        { config.allowUnfree = true;
-        }
+nixpkgs ? import
+
+#------------------------------------------------------------------------------
+# Modify the version in this line to change the nixpkgs version
+#------------------------------------------------------------------------------
+
+(builtins.fetchTarball
+  https://github.com/NixOS/nixpkgs/archive/refs/tags/branch-off-24.11.tar.gz
+)
+
+#------------------------------------------------------------------------------
+# nixpkgs options
+#------------------------------------------------------------------------------
+
+{
+  config.allowUnfree = true; # Allow unfree packages for some vscode extensions
+}
+
+#------------------------------------------------------------------------------
+# Optional arguments to the nix-shell
+#------------------------------------------------------------------------------
+
+# CAUTION! a spelling mistake in an arg string is ignored silently.
+
+# To use a specific ghc version: nix-shell --argstr compiler "ghc966"
 , compiler ? "ghc96"
+
+# To disable installation of editors: nix-shell --arg editors false
 , editors ? true
+
+# To disable installation haskell tools (hls etc):
+# nix-shell --arg haskell-tools false
 , haskell-tools ? true
+
+# To disable hoogle search engine database: nix-shell --arg hoogle false
 , hoogle ? true
-, all ? false
+
 }:
 
 let
 
 #------------------------------------------------------------------------------
-# Packages available in the nix shell
+# Haskell libraries, packages pre-installed in the nix shell
+# Add or remove as you wish, any hackage package in nixpkgs set can be used.
 #------------------------------------------------------------------------------
 
-    haskellLibDeps =
-      with hpkgs;
-        [ # Streamly packages
-          fusion-plugin
-          streamly
-          streamly-bytestring
-          streamly-core
-          streamly-coreutils
-          #streamly-examples
-          streamly-filepath
-          streamly-fsevents
-          #streamly-lz4
-          #streamly-metrics
-          streamly-process
-          streamly-statistics
-          streamly-text
+installedDeps =
+  with hpkgs;
+    [ # Streamly packages
+      fusion-plugin
+      streamly
+      streamly-bytestring
+      streamly-core
+      streamly-coreutils
+      streamly-filepath
+      streamly-fsevents
+      streamly-process
+      streamly-statistics
+      streamly-text
 
-          ## Additional packages
-          hspec
-          tasty-bench
-          ghczdecode
+      ## Additional packages
+      hspec
+      tasty-bench
+      ghczdecode
+    ];
+
+#------------------------------------------------------------------------------
+# The versions of Haskell packages used are defined in nix/haskellPackages.nix
+#------------------------------------------------------------------------------
+
+haskellPackages =
+  (import ./nix/haskellPackages.nix)
+    { inherit nixpkgs;
+      inherit compiler;
+    };
+
+#------------------------------------------------------------------------------
+# TODO
+#------------------------------------------------------------------------------
+
+hpkgs =
+  haskellPackages.override (old: {
+    overrides =
+      nixpkgs.lib.composeExtensions
+        (old.overrides or (_: _: {}))
+        (with nixpkgs.haskell.lib; self: super: {
+          # Add local packages here
+          # XXX Uses src = null, need to fix
+          #streamly = local super "streamly" ./. flags inShell;
+        });
+  });
+
+#------------------------------------------------------------------------------
+# Vim editor configuration
+#------------------------------------------------------------------------------
+
+vimCfg = import nix/vim/vim.nix {nixpkgs = nixpkgs;};
+
+#------------------------------------------------------------------------------
+# VScode editor configuration
+#------------------------------------------------------------------------------
+
+vscodium =
+  nixpkgs.pkgs.vscode-with-extensions.override
+    { vscode = nixpkgs.pkgs.vscodium;
+      vscodeExtensions =
+        with nixpkgs.pkgs.vscode-extensions;
+        # Extensions from
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/vscode/extensions/default.nix
+        [
+          # Needs to be installed explicitly for haskell.haskell to work
+          justusadam.language-haskell
+          haskell.haskell
+          #ms-vscode-remote.remote-ssh # unfree
+        ]
+        # Extensions from marketplace
+        ++ nixpkgs.pkgs.vscode-utils.extensionsFromVscodeMarketplace
+        [
+          #{
+          #  name = "";
+          #  publisher = "";
+          #  version = "";
+          #  sha256 = "";
+          #}
         ];
-
-    vimCfg = import nix/vim/vim.nix {nixpkgs = nixpkgs;};
-    vscodium =
-      nixpkgs.pkgs.vscode-with-extensions.override
-        { vscode = nixpkgs.pkgs.vscodium;
-          vscodeExtensions =
-            with nixpkgs.pkgs.vscode-extensions;
-            # Extensions from
-            # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/vscode/extensions/default.nix
-            [
-              # Needs to be installed explicitly for haskell.haskell to work
-              justusadam.language-haskell
-              haskell.haskell
-              #ms-vscode-remote.remote-ssh # unfree
-            ]
-            # Extensions from marketplace
-            ++ nixpkgs.pkgs.vscode-utils.extensionsFromVscodeMarketplace
-            [
-              #{
-              #  name = "";
-              #  publisher = "";
-              #  version = "";
-              #  sha256 = "";
-              #}
-            ];
-        };
-
-    otherPackages =
-        [ nixpkgs.pkgs.cabal-install
-
-          nixpkgs.pkgs.par                     # paragraph formatting for vim
-          nixpkgs.pkgs.powerline-fonts         # for vim status line
-          vimCfg.nvimCustom
-        ] ++
-          ( if (all || haskell-tools)
-            then
-            [ #nixpkgs.pkgs.hlint
-              #nixpkgs.haskellPackages.fourmolu
-              #nixpkgs.pkgs.ghcid
-              hpkgs.haskell-language-server
-            ] else []
-          ) ++
-          ( if (all || editors)
-            then [ vscodium ]
-            else []
-          );
+    };
 
 #------------------------------------------------------------------------------
-# Generic stuff
+# other packages: haskell tools, editors, language server etc.
 #------------------------------------------------------------------------------
 
-    haskellPackages =
-      (import ./nix/haskellPackages.nix)
-        { inherit nixpkgs;
-          inherit compiler;
-        };
+otherPackages =
+    [ nixpkgs.pkgs.cabal-install
+      nixpkgs.pkgs.par                     # paragraph formatting for vim
+      nixpkgs.pkgs.powerline-fonts         # for vim status line
+      vimCfg.nvimCustom
+    ] ++
+      ( if (haskell-tools)
+        then
+        [ #nixpkgs.pkgs.hlint
+          #nixpkgs.haskellPackages.fourmolu
+          #nixpkgs.pkgs.ghcid
+          hpkgs.haskell-language-server
+        ] else []
+      ) ++
+      ( if (editors)
+        then [ vscodium ]
+        else []
+      );
 
-    hpkgs =
-      haskellPackages.override (old: {
-        overrides =
-          nixpkgs.lib.composeExtensions
-            (old.overrides or (_: _: {}))
-            (with nixpkgs.haskell.lib; self: super: {
-              # Add local packages here
-              # XXX Uses src = null, need to fix
-              #streamly = local super "streamly" ./. flags inShell;
-            });
-      });
+#------------------------------------------------------------------------------
+# Additional library, setup and executable dependencies
+#------------------------------------------------------------------------------
 
-    # A fake package to add some additional deps to the shell env
-    additionalDeps = hpkgs.mkDerivation rec {
-              version = "0.1";
-              pname   = "streamly-additional";
-              license = "BSD-3-Clause";
+# A fake package to add some additional deps to the shell env
+additionalDeps = hpkgs.mkDerivation rec {
+    version = "0.1";
+    pname   = "streamly-additional";
+    license = "BSD-3-Clause";
 
-              libraryHaskellDepends = haskellLibDeps;
-              setupHaskellDepends = with hpkgs; [
-                cabal-doctest
-              ];
-              executableFrameworkDepends = with hpkgs;
-                # XXX On macOS cabal2nix does not seem to generate a
-                # dependency on Cocoa framework.
-                if builtins.currentSystem == "x86_64-darwin"
-                then [nixpkgs.darwin.apple_sdk.frameworks.Cocoa]
-                else [];
-            };
+    libraryHaskellDepends = installedDeps;
+    setupHaskellDepends = with hpkgs; [
+      cabal-doctest
+    ];
+    executableFrameworkDepends = with hpkgs;
+      # XXX On macOS cabal2nix does not seem to generate a
+      # dependency on Cocoa framework.
+      if builtins.currentSystem == "x86_64-darwin"
+      then [nixpkgs.darwin.apple_sdk.frameworks.Cocoa]
+      else [];
+};
 
-    utils =
-      (import ./nix/utils.nix)
-        { inherit nixpkgs; };
+#------------------------------------------------------------------------------
+# Import nix utility functions
+#------------------------------------------------------------------------------
+
+utils = (import ./nix/utils.nix) { inherit nixpkgs; };
+
+#------------------------------------------------------------------------------
+# Define the nix shell using haskell packages, additional deps and other
+# packages.
+#------------------------------------------------------------------------------
 
 in if nixpkgs.lib.inNixShell
-   then
-      utils.mkShell
-        hpkgs (p: [additionalDeps]) otherPackages (all || hoogle) true
+   then utils.mkShell hpkgs (p: [additionalDeps]) otherPackages hoogle true
    else abort "nix-shell only please!"
