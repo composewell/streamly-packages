82d3c0b0c6d9d110c6adcc127fdf31d8596910b6
Harendra Kumar <harendra@composewell.com>
2025-09-27 03:29:03 +0530

    Re-add plugins removed by refactor

--- nix/vim/plugins.nix
+++ nix/vim/plugins.nix
@@ -30,12 +30,13 @@ with pkgs.vimPlugins; {
     vim-commentary
 
     # Completion/Language server
+    #supertab
     coc-nvim
 
     # tmux
     vim-tmux-navigator
-    tslime
-    vim-tbone
+    tslime # https://github.com/jgdavey/tslime.vim
+    vim-tbone # yank and paste to/from tmux buffers
 
     # Git
     gitignore-vim
@@ -43,27 +44,41 @@ with pkgs.vimPlugins; {
     vim-extradite
 
     # Haskell
-    haskell-vim
-    vim-hoogle
-    hlint-refactor-vim
+    haskell-vim             #, { 'for': 'haskell' }
+    #vim-haskellConcealPlus  #, { 'for': 'haskell' }
+    vim-hoogle              #, { 'for': 'haskell' }
+    hlint-refactor-vim      #, { 'for': 'haskell' }
 
     # Nix
     vim-addon-nix
+
+    #vim-bracketed-paste
+    #vimproc-vim
+    #InstantRst
+    #vim-instant-markdown' # , { 'for': 'markdown'} " markdown WYSIWYG in browser
+    #diffchar causes random characters displayed very often - blacklisted
+    #diffchar.vim
   ];
 
-  # Optional plugins: loaded manually with :packadd
+  # Optional plugins: can be added using `:packadd $plugin-name`
+  # To automatically load a plugin when opening a filetype, add
+  # vimrc lines like: autocmd FileType php :packadd phpCompletion
   opt = [
+    # YouCompleteMe causes download of more than 500 MB, taking more
+    # than 2GB space on disk.
+    # Slows startup, so we load it on insert
+    # YouCompleteMe #, { 'on': [] }
     delimitMate
     ultisnips
-    vim-snippets
+    vim-snippets # Snippets repository
 
     # Web Development
-    tern_for_vim
-    vim-jsbeautify
-    vim-javascript
-    #purescript-vim
+    tern_for_vim        # { 'for': 'javascript'} JS completions
+    vim-jsbeautify      # { 'for': 'javascript'}
+    vim-javascript      # { 'for': 'javascript'}
+    #purescript-vim      # { 'for': 'purescript'}
 
-    python-mode
-    #vim-scala
+    python-mode # { 'for': 'python'}
+    #vim-scala   # { 'for': 'scala'}
   ];
 }
