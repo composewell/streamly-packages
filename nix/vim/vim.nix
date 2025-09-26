2512e97fc2ab5478f032f4c6793a936928329d2b
Harendra Kumar <harendra@composewell.com>
2025-09-27 03:15:33 +0530

    Refactor vim.nix

--- nix/vim/vim.nix
+++ nix/vim/vim.nix
@@ -1,28 +1,37 @@
 # Copyright (c) 2022 Composewell Technologies
 
-{nixpkgs}:
-with import ../utils.nix {nixpkgs = nixpkgs;};
-let plugins = import ./plugins.nix { pkgs = nixpkgs.pkgs; };
+{ nixpkgs }:
+with import ../utils.nix { inherit nixpkgs; };
 
-    #vimCustom = nixpkgs.vim_configurable.customize {
-    #  name = "vim";
-    #  vimrcConfig.customRC = builtins.readFile ./vimrc;
-    #  vimrcConfig.packages.myVimPackage = plugins;
-    #};
+let
+  # Load plugins from a separate file
+  plugins = import ./plugins.nix { pkgs = nixpkgs.pkgs; };
 
-    cheatsheet = nixpkgs.writeTextFile {
-        name = "cheatsheet-vim";
-        text = builtins.readFile ./cheatsheet.txt;
-        executable = false;
-        destination = "/etc/cheatsheet.txt";
+  # Small helper: write a plain text file into /etc
+  etcTextFile = name: path:
+    nixpkgs.writeTextFile {
+      inherit name;
+      text = builtins.readFile path;
+      executable = false;
+      destination = "/etc/${name}";
     };
 
+  cheatsheet = etcTextFile "cheatsheet.txt" ./cheatsheet.txt;
 in {
-#------------------------------------------------------------------------------
-#  nvim with plugins
-#------------------------------------------------------------------------------
 
-nvimCustom = nixpkgs.neovim.override {
+  #------------------------------------------------------------------------------
+  #  Generic vim/neovim configuration system
+  #------------------------------------------------------------------------------
+  #vimCustom = nixpkgs.vim_configurable.customize {
+  #  name = "vim";
+  #  vimrcConfig.customRC = builtins.readFile ./vimrc;
+  #  vimrcConfig.packages.myVimPackage = plugins;
+  #};
+
+  #------------------------------------------------------------------------------
+  #  nvim with plugins
+  #------------------------------------------------------------------------------
+  nvimCustom = nixpkgs.neovim.override {
     configure = {
       customRC =
            builtins.readFile ./vimrc
@@ -32,24 +41,17 @@ nvimCustom = nixpkgs.neovim.override {
         + "let g:coc_node_path='${nixpkgs.nodejs}/bin/node'";
       packages.myVimPackage = plugins;
     };
-};
-
-#------------------------------------------------------------------------------
-# bashrc
-#------------------------------------------------------------------------------
-
-bashrc = writeShellScriptTo "bashrc_vim" "/etc/bashrc.d/01_vim" (builtins.readFile ./bashrc);
-
-#------------------------------------------------------------------------------
-# gitconfig
-#------------------------------------------------------------------------------
-
-gitconfig =
-  let gitCfg = builtins.readFile ./gitconfig;
-  in nixpkgs.writeTextFile {
-      name = "gitconfig-vim";
-      text = gitCfg;
-      executable = false;
-      destination = "/etc/gitconfig.vim";
   };
+
+  #------------------------------------------------------------------------------
+  # bashrc
+  #------------------------------------------------------------------------------
+  bashrc =
+    writeShellScriptTo
+      "bashrc_vim" "/etc/bashrc.d/01_vim" (builtins.readFile ./bashrc);
+
+  #------------------------------------------------------------------------------
+  # gitconfig
+  #------------------------------------------------------------------------------
+  gitconfig = etcTextFile "gitconfig.vim" ./gitconfig;
 }
