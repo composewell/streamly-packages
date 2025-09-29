ed3673597d8d7f36d6f09f022ddb2dac7cb41466
Harendra Kumar <harendra@composewell.com>
2025-09-30 01:10:50 +0530

    Move most of shell.nix to src/env.nix

--- shell.nix
+++ shell.nix
@@ -40,116 +40,17 @@ in
 # To use a specific ghc version: nix-shell --argstr compiler "ghc966"
 , compiler ? "default"
 
-# To disable installation of editors: nix-shell --arg editors false
-#, editors ? true
-
-# To disable installation haskell tools (hls etc):
-# nix-shell --arg haskell-tools false
-#, haskell-tools ? true
-
 # To disable hoogle search engine database: nix-shell --arg hoogle false
 , hoogle ? true
 
 }:
 
 let
-
-#------------------------------------------------------------------------------
-# Import nix utility functions
-#------------------------------------------------------------------------------
-
 system = builtins.currentSystem;
-utils = (import ./src/utils.nix) { inherit nixpkgs; inherit system; };
-utils1 = (import ./src/make-overrides.nix) { inherit nixpkgs; inherit system; };
-
-#------------------------------------------------------------------------------
-# The versions of Haskell packages used are defined in nix/haskellPackages.nix
-#------------------------------------------------------------------------------
-
-haskellPackages =
-let
-    sources = import ./sources.nix;
-
-    hpkgs =
-        if compiler == "default"
-        then nixpkgs.haskellPackages
-        else nixpkgs.haskell.packages.${compiler};
-
-    #recompile = pkg:
-    #    pkg.overrideAttrs (oldAttrs:
-    #      { doCheck = false;
-    #        #doHaddock = false;
-    #        configureFlags =
-    #          oldAttrs.configureFlags ++ ["--disable-tests"];
-    #      });
-
-in
-    hpkgs.override {
-      overrides = self: super:
-        utils1.makeOverrides super sources;
-    };
-
-#------------------------------------------------------------------------------
-# TODO
-#------------------------------------------------------------------------------
-
-#haskellPackages1 =
-#  haskellPackages.override (old: {
-#    overrides =
-#      nixpkgs.lib.composeExtensions
-#        (old.overrides or (_: _: {}))
-#        (with nixpkgs.haskell.lib; self: super: {
-#          # Add local packages here
-#          # XXX Uses src = null, need to fix
-#          #streamly = local super "streamly" ./. flags inShell;
-#        });
-#  });
-
-#------------------------------------------------------------------------------
-# Vim editor configuration
-#------------------------------------------------------------------------------
-
-vimCfg = import vim/vim.nix {nixpkgs = nixpkgs;};
-
-#------------------------------------------------------------------------------
-# VScode editor configuration
-#------------------------------------------------------------------------------
-
-vscodiumCfg = import vscodium/vscodium.nix {inherit nixpkgs;};
-
-#------------------------------------------------------------------------------
-# other packages: haskell tools, editors, language server etc.
-#------------------------------------------------------------------------------
-
-otherPackages = import ./packages.nix
-  { pkgs = nixpkgs.pkgs;
-    hpkgs = haskellPackages;
-    vim = vimCfg.nvimCustom;
-    vscodium = vscodiumCfg.vscodium;
-  };
-
-#------------------------------------------------------------------------------
-# Additional library, setup and executable dependencies
-#------------------------------------------------------------------------------
-
-# A fake package to add some additional deps to the shell env
-additionalDeps = haskellPackages.mkDerivation rec {
-    version = "0.1";
-    pname   = "streamly-additional";
-    license = "BSD-3-Clause";
-
-    libraryHaskellDepends = otherPackages.libraries;
-    setupHaskellDepends = with haskellPackages; [
-      cabal-doctest
-    ];
-    # XXX On macOS cabal2nix does not seem to generate a
-    # dependency on Cocoa framework.
-    executableFrameworkDepends = utils.cocoa;
-};
-
-#------------------------------------------------------------------------------
-# Define the nix shell using haskell packages, additional deps and other
-# packages.
-#------------------------------------------------------------------------------
-
-in utils.mkShell haskellPackages (p: [additionalDeps]) otherPackages.packages hoogle true
+env = import ./src/env.nix {
+            inherit nixpkgs;
+            inherit compiler;
+            inherit system;
+            inherit hoogle;
+          };
+in env
