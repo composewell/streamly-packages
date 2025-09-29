ad928b6d4d8f94119aefe941acf091c690a3f862
Harendra Kumar <harendra@composewell.com>
2025-09-30 01:28:08 +0530

    Refactor, cleanup src/env.nix

--- src/env.nix
+++ src/env.nix
@@ -1,32 +1,25 @@
 { nixpkgs, compiler, system, hoogle }:
 let
 
-utils = (import ./utils.nix) { inherit nixpkgs; inherit system; };
-utils1 = (import ./make-overrides.nix) { inherit nixpkgs; inherit system; };
+  utils = (import ./utils.nix) {
+    inherit nixpkgs;
+    inherit system;
+  };
 
-#------------------------------------------------------------------------------
-# The versions of Haskell packages used are defined in nix/haskellPackages.nix
-#------------------------------------------------------------------------------
+  utils1 = (import ./make-overrides.nix) {
+    inherit nixpkgs;
+    inherit system;
+  };
+
+  haskellPackagesOrig = if compiler == "default" then
+    nixpkgs.haskellPackages
+  else
+    nixpkgs.haskell.packages.${compiler};
+
+  sources = import ../sources.nix;
 
 haskellPackages =
-let
-    sources = import ../sources.nix;
-
-    hpkgs =
-        if compiler == "default"
-        then nixpkgs.haskellPackages
-        else nixpkgs.haskell.packages.${compiler};
-
-    #recompile = pkg:
-    #    pkg.overrideAttrs (oldAttrs:
-    #      { doCheck = false;
-    #        #doHaddock = false;
-    #        configureFlags =
-    #          oldAttrs.configureFlags ++ ["--disable-tests"];
-    #      });
-
-in
-    hpkgs.override {
+    haskellPackagesOrig.override {
       overrides = self: super:
         utils1.makeOverrides super sources;
     };
@@ -35,47 +28,34 @@ in
 # Vim editor configuration
 #------------------------------------------------------------------------------
 
-vimCfg = import ../vim/vim.nix {nixpkgs = nixpkgs;};
+  vimCfg = import ../vim/vim.nix {nixpkgs = nixpkgs;};
 
 #------------------------------------------------------------------------------
 # VScode editor configuration
 #------------------------------------------------------------------------------
 
-vscodiumCfg = import ../vscodium/vscodium.nix {inherit nixpkgs;};
+  vscodiumCfg = import ../vscodium/vscodium.nix {inherit nixpkgs;};
 
-#------------------------------------------------------------------------------
-# other packages: haskell tools, editors, language server etc.
-#------------------------------------------------------------------------------
+  requiredPackages = import ../packages.nix
+    { pkgs = nixpkgs.pkgs;
+      hpkgs = haskellPackages;
+      vim = vimCfg.nvimCustom;
+      vscodium = vscodiumCfg.vscodium;
+    };
 
-otherPackages = import ../packages.nix
-  { pkgs = nixpkgs.pkgs;
-    hpkgs = haskellPackages;
-    vim = vimCfg.nvimCustom;
-    vscodium = vscodiumCfg.vscodium;
-  };
-
-#------------------------------------------------------------------------------
-# Additional library, setup and executable dependencies
-#------------------------------------------------------------------------------
-
-# A fake package to add some additional deps to the shell env
-additionalDeps = haskellPackages.mkDerivation rec {
+  # A fake package to add some additional deps to the shell env
+  additionalDeps = haskellPackages.mkDerivation rec {
     version = "0.1";
-    pname   = "streamly-additional";
+    pname = "fake-shell-deps";
     license = "BSD-3-Clause";
 
-    libraryHaskellDepends = otherPackages.libraries;
-    setupHaskellDepends = with haskellPackages; [
-      cabal-doctest
-    ];
+    libraryHaskellDepends = requiredPackages.libraries;
+    setupHaskellDepends = with haskellPackages; [ cabal-doctest ];
     # XXX On macOS cabal2nix does not seem to generate a
     # dependency on Cocoa framework.
     executableFrameworkDepends = utils.cocoa;
-};
+  };
 
-#------------------------------------------------------------------------------
-# Define the nix shell using haskell packages, additional deps and other
-# packages.
-#------------------------------------------------------------------------------
+  shell = utils.mkShell haskellPackages (p: [ additionalDeps ]) requiredPackages.packages hoogle true;
 
-in utils.mkShell haskellPackages (p: [additionalDeps]) otherPackages.packages hoogle true
+in shell
