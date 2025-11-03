a00eb18e3776ebe5186e5923e4b1132807621de2
Harendra Kumar <harendra@composewell.com>
2025-11-03 17:29:55 +0530

    Update nixpack commit

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     #nixpkgs-darwin.url = "github:NixOS/nixpkgs/e99366c665bdd53b7b500ccdc5226675cfc51f45"; # nixpkgs-unstable
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/08478b816182dc3cc208210b996294411690111d"; # nixpkgs-25.05-darwin
     # For local testing use "path:.../nixpack";
-    basepkgs.url = "github:composewell/nixpack/796a47f5d9abd9f66c9340e2b9ff9aeed2af1389";
+    basepkgs.url = "github:composewell/nixpack/1d63fd85b20f9322c4cd868da9b25eee6365cdc4";
   };
 
   outputs = { self, nixpkgs, nixpkgs-darwin, basepkgs }:
